772773e2aa646739f47d0cca6a02182a3537004a
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-27 20:09:09 +0530

    feat: update layout and add header component for improved navigation

---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./ModeToggle";
+import { cn } from "@/lib/utils";
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = React.useState(false);
+
+  // Listener to scroll events to hide the header on scroll down
+  React.useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      className={cn(
+        "fixed top-0 right-0 left-0 z-100 container mx-auto flex h-[40px] items-center justify-between py-2",
+        {
+          "bg-white/50 shadow-md backdrop-blur-md": isScrolled,
+          "bg-transparent": !isScrolled,
+        },
+      )}
+    >
+      <div className="space-x-4">
+        <h1 className="text-2xl font-bold">Simple Bank</h1>
+      </div>
+      <div className="flex items-center space-x-2">
+        <ModeToggle />
+        <Button>Temp</Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
