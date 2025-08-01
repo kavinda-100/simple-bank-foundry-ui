931899df7856d2437f8d800fab4d7de38eee53b5
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-01 12:38:43 +0530

    feat: add Card, Input, and Label components for enhanced UI structure and functionality

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,7 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import React from "react";
 
 const page = () => {
-  return <div className="size-full">page</div>;
+  return (
+    <div className="flex size-full flex-col items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Create a New Account</CardTitle>
+          <CardDescription>
+            Please fill in the details below to create a new account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+        <CardFooter></CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default page;
